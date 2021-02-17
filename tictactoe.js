2df73993f1846c6622bce13a1aeda806921927f3
unknown <eexdhe@gmail.com>
2021-02-17 18:29:32 +0100

    implement a simple version of minmax algorithm

--- tictactoe.js
+++ tictactoe.js
@@ -2,10 +2,19 @@ class Tictactoe {
   constructor(_element, _pawns) {
     this.element = _element;
     this.cells = [..._element.querySelectorAll(".board-cell")];
-    this.board = Array(9).fill(-1);
+    this.board = [
+      [-1, -1, -1],
+      [-1, -1, -1],
+      [-1, -1, -1],
+    ];
     this.playerTurn = 0;
     this.pawns = _pawns;
-    this.reset = document.querySelectorAll('.reset');
+    this.reset = document.querySelectorAll(".reset");
+
+    this.evaluate = this.evaluate.bind(this);
+    this.findBestMove = this.findBestMove.bind(this);
+    this.minmax = this.minmax.bind(this);
+    this.checkWin = this.checkWin.bind(this);
 
     this.handleClick = this.handleClick.bind(this);
 
@@ -14,72 +23,177 @@ class Tictactoe {
     );
 
     this.handleReset = this.handleReset.bind(this);
-    
+
     this.reset.forEach((btn) => {
-      btn.addEventListener('click', this.handleReset);
+      btn.addEventListener("click", this.handleReset);
     });
   }
 
-  evaluate(){
-    const possibleWinCombination = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 6, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+  evaluate() {
+    let isWin = this.checkWin();
 
-    let isWin = false;
-
-    // We check if there is a winning combination
-    possibleWinCombination.forEach((combination) => {
-      let consecutiveCell = 0;
-      combination.forEach((cellIndex) => {
-        if (this.board[cellIndex] === this.playerTurn) {
-          ++consecutiveCell;
-        }
-      });
-
-      if (consecutiveCell === 3) {
-        isWin = true;
+    if (isWin) {
+      if (this.playerTurn === 0) {
+        return 10;
+      } else if (this.playerTurn === 1) {
+        return -10;
       }
-    });
+    }
 
-    let isDraw = this.draw();
-
-    if(isWin){
-      return (this.playerTurn === 0) ? 10 : -10;
-    }else if(isDraw){
+    let isDraw = this.checkDraw();
+    if (isDraw) {
       return 0;
     }
   }
 
+  minmax(depth, isMax) {
+    let score = this.evaluate(this.board);
+
+    if (score === 10 || score === -10) {
+      return score;
+    }
+
+    if (!this.checkDraw()) {
+      if (isMax) {
+        let best = -Infinity;
+
+        for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 3; j++) {
+            if (this.board[i][j] === -1) {
+              this.board[i][j] = 0;
+
+              best = Math.max(best, this.minmax(depth + 1, !isMax));
+
+              this.board[i][j] = -1;
+            }
+          }
+
+          return best;
+        }
+      }
+    } else {
+      let best = +Infinity;
+
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Check if cell isn't empty
+          if (this.board[i][j] === -1) {
+            this.board[i][j] = 0;
+
+            best = Math.min(best, minmax(depth + 1, !isMax));
+
+            this.board[i][j] = -1;
+          }
+        }
+      }
+
+      return best;
+    }
+  }
+
+  findBestMove() {
+    let bestVal = -Infinity;
+    let move = [-1, -1];
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.board[i][j] === -1) {
+          this.board[i][j] = 0;
+
+          let moveVal = this.minmax(0, true);
+
+          this.board[i][j] = -1;
+
+          if (moveVal > bestVal) {
+            move = [i, j];
+            bestVal = moveVal;
+          }
+        }
+      }
+    }
+
+    let row = move[0];
+    let col = move[1];
+    if (move[0] === -1 && move[1] === -1) {
+      const cell = this.chooseRandomCell();
+
+      row = cell[0];
+      col = cell[1];
+    }
+
+    this.board[row][col] = 1;
+
+    let cell = null;
+    this.cells.forEach((temp) => {
+      if(temp.dataset.rowIndex == row && temp.dataset.colIndex == col){
+        cell = temp;
+      }
+    })
+
+    const img = this.choosePawnImg();
+    cell.appendChild(img);
+  }
+
+  chooseRandomCell() {
+    let row = Math.floor(Math.random() * 3);
+    let col = Math.floor(Math.random() * 3);
+
+    let isOccupied = false;
+
+    if (this.board[row][col] != -1) {
+      isOccupied = true;
+    }
+
+    while (isOccupied) {
+      row = Math.floor(Math.random() * 3);
+      col = Math.floor(Math.random() * 3);
+
+      if (this.board[row][col] == -1) {
+        isOccupied = false;
+      }
+    }
+
+    return [row, col];
+  }
+
   handleClick(e) {
     const currentCell = e.target;
+    const col = +currentCell.getAttribute("data-col-index");
+    const row = +currentCell.getAttribute("data-row-index");
 
     // Check if there is not already a pawn
     if (this.isEmpty(currentCell)) {
       const img = this.choosePawnImg();
       currentCell.appendChild(img);
 
-      const cellIndex = this.getCellIndex(currentCell);
-      this.board[cellIndex] = this.playerTurn;
+      this.board[row][col] = 0;
+      
+      let isWin = this.checkWin();
+      let isDraw = this.checkDraw();
+      
+      if(isWin){
+        return this.win();
+      }
+      
+      if(isDraw){
+        return this.draw();
+      }
+      this.playerTurn = 1;
 
-      this.checkResult();
+      this.findBestMove();
 
-      this.playerTurn = +!this.playerTurn;
-    }
-  }
+      isWin = this.checkWin();
+      isDraw = this.checkDraw();
 
-  // Check if there is a win or a draw
-  checkResult() {
-    if (this.checkWin()) {
-      return this.win();
-    } else if (this.checkDraw()) {
-      return this.draw();
+      if(isWin){
+        return this.win();
+      }
+
+      if(isDraw){
+        return this.draw();
+      }
+
+      this.playerTurn = 0;
     }
   }
 
@@ -90,79 +204,108 @@ class Tictactoe {
     this.showModal();
   }
 
-  setModal(msg, img){
-    const resultMsg = document.querySelector('.result-msg');
+  setModal(msg, img) {
+    const resultMsg = document.querySelector(".result-msg");
     resultMsg.textContent = msg;
 
-    const resultImg = document.querySelector('.result-img');
-    resultImg.setAttribute('src', `img/${img}`);
+    const resultImg = document.querySelector(".result-img");
+    resultImg.setAttribute("src", `img/${img}`);
   }
 
-  showModal(){
-    const modalWrapper = document.querySelector('.modal-wrapper');
+  showModal() {
+    const modalWrapper = document.querySelector(".modal-wrapper");
 
-    modalWrapper.style.display = 'flex';
+    modalWrapper.style.display = "flex";
   }
 
-  closeModal(){
-    const modalWrapper = document.querySelector('.modal-wrapper');
-    modalWrapper.style.display = 'none';
+  closeModal() {
+    const modalWrapper = document.querySelector(".modal-wrapper");
+    modalWrapper.style.display = "none";
   }
 
   draw() {
-    const msg = 'Draw';
-    const img = 'equal.svg';
+    const msg = "Draw";
+    const img = "equal.svg";
     this.setModal(msg, img);
     this.showModal();
   }
 
   // reset the game
-  handleReset(){
-    this.board = Array(9).fill(-1);
+  handleReset() {
+    this.board = [
+      [-1, -1, -1],
+      [-1, -1, -1],
+      [-1, -1, -1],
+    ];
     this.closeModal();
 
     this.cells.forEach((cell) => {
-      const img = cell.querySelector('img');
-      if(img){
+      const img = cell.querySelector("img");
+      if (img) {
         cell.removeChild(img);
       }
     });
+
+    this.playerTurn = 0;
   }
 
   checkWin() {
-    // all the winning combination
-    const possibleWinCombination = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 6, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    let isWin = false;
-
-    // We check if there is a winning combination
-    possibleWinCombination.forEach((combination) => {
-      let consecutiveCell = 0;
-      combination.forEach((cellIndex) => {
-        if (this.board[cellIndex] === this.playerTurn) {
-          ++consecutiveCell;
+    // Check rows
+    for (let row = 0; row < 3; row++) {
+      if (
+        this.board[row][0] === this.board[row][1] &&
+        this.board[row][1] === this.board[row][2]
+      ) {
+        if (this.board[row][0] !== -1) {
+          return true;
         }
-      });
-
-      if (consecutiveCell === 3) {
-        isWin = true;
       }
-    });
+    }
 
-    return isWin;
+    // Check columns
+    for (let col = 0; col < 3; col++) {
+      if (
+        this.board[0][col] === this.board[1][col] &&
+        this.board[1][col] === this.board[2][col]
+      ) {
+        if (this.board[0][col] !== -1) {
+          return true;
+        }
+      }
+    }
+
+    // check diagonals.
+    if (
+      this.board[0][0] === this.board[1][1] &&
+      this.board[1][1] === this.board[2][2]
+    ) {
+      if (this.board[0][0] !== -1) {
+        return true;
+      }
+    }
+
+    if (
+      this.board[0][2] === this.board[1][1] &&
+      this.board[1][1] === this.board[2][0]
+    ) {
+      if (this.board[0][2] !== -1) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   checkDraw() {
-    return this.board.every((cell) => cell !== -1);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.board[i][j] === -1) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
   choosePawnImg() {
@@ -173,13 +316,10 @@ class Tictactoe {
     return img;
   }
 
-  getCellIndex(cell) {
-    return +cell.getAttribute("data-cell-number");
-  }
-
   isEmpty(cell) {
-    const cellIndex = +cell.getAttribute("data-cell-number");
+    const col = +cell.getAttribute("data-col-index");
+    const row = +cell.getAttribute("data-row-index");
 
-    return this.board[cellIndex] === -1;
+    return this.board[row][col] === -1;
   }
 }
