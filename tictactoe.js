437ca4c3df70620b71f16ccb44dbf5dcab0840b9
unknown <eexdhe@gmail.com>
2021-02-17 14:53:44 +0100

    create the evaluate function

--- tictactoe.js
+++ tictactoe.js
@@ -20,6 +20,43 @@ class Tictactoe {
     });
   }
 
+  evaluate(){
+    const possibleWinCombination = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 6, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    let isWin = false;
+
+    // We check if there is a winning combination
+    possibleWinCombination.forEach((combination) => {
+      let consecutiveCell = 0;
+      combination.forEach((cellIndex) => {
+        if (this.board[cellIndex] === this.playerTurn) {
+          ++consecutiveCell;
+        }
+      });
+
+      if (consecutiveCell === 3) {
+        isWin = true;
+      }
+    });
+
+    let isDraw = this.draw();
+
+    if(isWin){
+      return (this.playerTurn === 0) ? 10 : -10;
+    }else if(isDraw){
+      return 0;
+    }
+  }
+
   handleClick(e) {
     const currentCell = e.target;
 
